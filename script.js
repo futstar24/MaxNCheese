794eeaf314a940afe15543c03b9db53a04dfce38
futstar24 <jelamberg@gmail.com>
2023-09-30 12:03:47 -0400

    add

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-shownMenuElement = document.getElementById("all")
+shownMenuElement = document.getElementById("home")
 
 Array.from(document.getElementsByClassName("menuElement")).forEach(menuElement => {
-    text = menuElement.innerHTML
-    menuElement.addEventListener("click", function() {
-        shownMenuElement.style.color = "#8F8F8E"
-        shownMenuElement = menuElement
-        shownMenuElement.style.color = "#444444"
-    })
+    if (menuElement.tagName == "P") {
+        console.log("hi")
+        text = menuElement.innerHTML
+        menuElement.addEventListener("click", function() {
+            shownMenuElement.style.color = "#8F8F8E"
+            shownMenuElement = menuElement
+            shownMenuElement.style.color = "#444444"
+        })
+    }
 })
