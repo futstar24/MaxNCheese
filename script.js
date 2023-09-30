ba78d69fb7ab31d4a92c7afa7e118bef230c7961
futstar24 <jelamberg@gmail.com>
2023-09-30 13:24:08 -0400

    add

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+
+Array.from(document.getElementsByClassName("albumGridElement")).forEach(albumGridElement => {
+    albumGridElement.addEventListener("click", function() {
+        albumChildren = albumGridElement.childNodes
+        text = ""
+        albumChildren.forEach(element => {
+            if (element.tagName == "P") {
+                text = element.innerHTML.toLowerCase().replace(/\s/g,"")
+            }
+        })
+        window.open(URL, "https://maxncheesephotography.pic-time.com/-"+text+"/gallery")
+    })
+})
+
+socials = document.getElementById("socials")
+socials.parentElement.removeChild(socials)
+pages = []
+
+Array.from(document.getElementsByClassName("page")).forEach(page => {
+    clonedSocials = socials.cloneNode(true)
+    page.appendChild(clonedSocials)
+    pages.push(page)
+    page.parentElement.removeChild(page)
+})
+
+
+currentPage = pages[0]
+
+document.getElementById("pageContent").appendChild(currentPage)
+
 shownMenuElement = document.getElementById("home")
 
 Array.from(document.getElementsByClassName("menuElement")).forEach(menuElement => {
@@ -5,9 +35,29 @@ Array.from(document.getElementsByClassName("menuElement")).forEach(menuElement =
         console.log("hi")
         text = menuElement.innerHTML
         menuElement.addEventListener("click", function() {
-            shownMenuElement.style.color = "#8F8F8E"
-            shownMenuElement = menuElement
-            shownMenuElement.style.color = "#444444"
+            if (menuElement != shownMenuElement) {
+                shownMenuElement.style.color = "#8F8F8E"
+                shownMenuElement = menuElement
+                shownMenuElement.style.color = "#444444"
+                showPage(shownMenuElement.innerHTML)
+            }
         })
     }
 })
+
+
+function showPage(pageDesc) {
+    removingPage = currentPage
+    removingPage.style.animation = "fadeOut 0.5s"
+    pages.forEach(page => {
+        if (page.id == pageDesc) {
+            currentPage = page
+        }
+    })
+    document.getElementById("pageContent").appendChild(currentPage)
+    currentPage.style.animation = "fadeIn 0.5s"
+
+    setTimeout(function(){
+        removingPage.parentElement.removeChild(removingPage)
+    }, "500")
+}
