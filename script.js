4133a0a46e9aeaebead7dfb480f30c5ffdce634e
futstar24 <jelamberg@gmail.com>
2023-10-01 22:43:53 -0400

    add

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function setTestimonials() {
     .then(text => {
         testimonialList = text.split("\n")
         testimonialCount = testimonialList.length
+        testimonialCount-=1
     })
 }
 
